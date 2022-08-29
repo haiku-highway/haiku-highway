83f652b28332e07efda9bab05f99cf8d633ceb45
CoreyDeCaire <coreydecaire19@gmail.com>
2022-08-29 17:52:55 -0400

    added CSS stylings for How to and credits page

--- src/Components/Credits.js
+++ src/Components/Credits.js
@@ -1,53 +1,53 @@
 const Credits = () => {
     return (
-        <section>
-            <h2>credits</h2>
-            <div>
-                <p>developed with datamuse api</p>
-                <p>
-                    by{" "}
-                    <a
-                    href="https://www.alixfaudot.com/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >
-                    alix
-                    </a>
-                    ,
-                    <a
-                    href="https://www.coreydecaire.com/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >
-                    corey
-                    </a>
-                    ,
-                    <a
-                    href="https://francesm.dev/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >
-                    frances
-                    </a>
-                    &#38;{" "}
-                    <a
-                    href="https://matthewrussell.dev/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >
-                    matt
-                    </a>
-                </p>
-                <p>
-                    at{" "}
-                    <a
-                    href="https://junocollege.com/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >
-                    juno college
-                    </a>
-                </p>
+        <section className="credits largePaper">
+            <div className="largePaperText">
+                <h2>credits</h2>
+                    <p>developed with datamuse api</p>
+                    <p>
+                        by{" "}
+                        <a className="alix"
+                        href="https://www.alixfaudot.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >
+                        alix
+                        </a>
+                        ,{" "}
+                        <a className="corey"
+                        href="https://www.coreydecaire.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >
+                        corey
+                        </a>
+                        ,{" "}
+                        <a className="frances"
+                        href="https://francesm.dev/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >
+                        frances
+                        </a>
+                        {" "}&#38;{" "}
+                        <a className="matt"
+                        href="https://matthewrussell.dev/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >
+                        matt
+                        </a>
+                    </p>
+                    <p>
+                        at{" "}
+                        <a className="juno"
+                        href="https://junocollege.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >
+                        juno college
+                        </a>
+                    </p>
             </div>
         </section>
     );
